8065382c0b389185ba302b47965c2c63d0a96433
jackman24 <andrew.jackman@bedegaming.com>
2018-04-20 09:25:17 +0100

    Nunit play and removing stupid comments

---
+++ BattleShipsBLL.Tests.Unit/Utilities/CoordinateTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BattleShipsBLL.Game.Utilities;
+using Moq;
+using NUnit.Framework;
+
+namespace BattleShipsBLL.Tests.Unit.Utilities
+{
+    [TestFixture]
+    class CoordinateTests
+    {
+        [TestCase("", 10, 65)]
+        [TestCase("123", 10, 65)]
+        [TestCase("AA", 10, 65)]
+        [TestCase("-1", 10, 65)]
+        [TestCase("##", -1, -1)]
+        [TestCase("B2", 2, 75)]
+        public void InvalidGridReference(string gridReference, int gameHeight, int asciiCharA)
+        {
+            Coordinate coordinate = new Coordinate();
+
+            Assert.IsFalse(Coordinate.ValidateGridReference(gridReference, ref coordinate, gameHeight, asciiCharA));
+        }
+    }
+}
